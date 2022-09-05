e1fde224b4205e8e626639759241b8089b19b0d5
AlexeyRachuk <alexeyrachuk94@gmail.com>
2022-09-05 22:29:19 +0700

    com 05.09

--- master_site/master_site/settings.py
+++ master_site/master_site/settings.py
@@ -91,10 +91,21 @@ WSGI_APPLICATION = 'master_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'agency_db',
+        'USER': 'agency_admin',
+        'PASSWORD': 'agencypass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
