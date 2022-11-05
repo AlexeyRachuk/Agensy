b2ffc28b966fc6038dc082c9431324facc4eba38
alekseyrachuk <alexeyrachuk94@gmail.com>
2022-11-05 22:31:12 +0700

    add footermenu template fix

--- master_site/master_site/settings.py
+++ master_site/master_site/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'master_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'agency_db',
-        'USER': 'agency_admin',
-        'PASSWORD': 'agencypass',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'agency_db',
+#        'USER': 'agency_admin',
+#        'PASSWORD': 'agencypass',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
